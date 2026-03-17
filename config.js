dab6f30509ea7e8275e4833e7344312cde2ab613
KP543 <you@example.com>
2026-03-17 14:12:34 +0800

    commit

---
+++ config.js
@@ -0,0 +1,8 @@
+// 企业微信配置
+module.exports = {
+  corpId: 'YOUR_CORP_ID',      // 企业ID
+  appId: 'YOUR_APP_ID',        // 应用ID
+  appSecret: 'YOUR_APP_SECRET', // 应用密钥
+  token: 'YOUR_TOKEN',          // 回调验证Token
+  encodingAESKey: 'YOUR_ENCODING_AES_KEY' // 加密密钥
+};
